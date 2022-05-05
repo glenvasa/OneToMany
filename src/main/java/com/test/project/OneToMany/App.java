7432ba80b33af6861618c19d1a88bfddb3d09a7c
glenvasa <glenvasa@gmail.com>
2022-05-05 10:01:47 -0400

    Update App.java

--- src/main/java/com/test/project/OneToMany/App.java
+++ src/main/java/com/test/project/OneToMany/App.java
@@ -1,15 +1,12 @@
 package com.test.project.OneToMany;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
-import com.perscholas.model.Departments;
-import com.perscholas.model.Teachers;
+import com.perscholas.model.Schools;
+import com.perscholas.model.Students;
 
 public class App {
 	public static void main(String[] args) {
@@ -18,45 +15,45 @@ public class App {
 
 		Transaction t = session.beginTransaction();
 
-		Teachers t1 = new Teachers();
-		t1.setTeachername("Haseeb");
-		t1.setSalary("100");
+		Schools school1 = new Schools();
+		school1.setSchoolName("Brockton High School");
 
-		Teachers t2 = new Teachers();
-		t2.setTeachername("Jenny Finch");
-		t2.setSalary("10000");
+		Schools school2 = new Schools();
+		school2.setSchoolName("Boston Latin");
 
-		Teachers t3 = new Teachers();
-		t3.setTeachername("James");
-		t3.setSalary("25000");
+		Students student1 = new Students();
+		student1.setStudentName("Jay");
+		student1.setCourseLoad(4);
+		student1.setSchools(school2);
 
-		Teachers t4 = new Teachers();
-		t4.setTeachername("SID ROSE");
-		t4.setSalary("3000");
+		Students student2 = new Students();
+		student2.setStudentName("Nicole");
+		student2.setCourseLoad(5);
+		student2.setSchools(school2);
 
-		Teachers t5 = new Teachers();
-		t5.setSalary("200");
-		t5.setTeachername("Ali");
+		Students student3 = new Students();
+		student3.setStudentName("Glen");
+		student3.setCourseLoad(2);
+		student3.setSchools(school1);
 
-		// Add Teachers entity object to the list
-		List<Teachers> teachlist = new ArrayList();
-		teachlist.add(t1);
-		teachlist.add(t2);
-		teachlist.add(t3);
-		teachlist.add(t4);
-		teachlist.add(t5);
-		session.save(t1);
-		session.save(t2);
-		session.save(t3);
-		session.save(t4);
+		Students student4 = new Students();
+		student4.setStudentName("Yakub");
+		student4.setCourseLoad(7);
+		student4.setSchools(school1);
 
-		// Create Department
-		Departments departments = new Departments();
-		departments.setDname("Development");
-		departments.setTeacherList(teachlist);
+		Students student5 = new Students();
+		student5.setStudentName("Aaron");
+		student5.setCourseLoad(7);
+		student5.setSchools(school2);
 
-		// Store Department
-		session.save(departments);
+		session.save(school1);
+		session.save(school2);
+		session.save(student1);
+		session.save(student2);
+		session.save(student3);
+		session.save(student4);
+		session.save(student5);
 		t.commit();
+
 	}
 }
